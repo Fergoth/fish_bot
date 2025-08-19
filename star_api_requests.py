941a9b030bd099df0092bed24ec312e7483cd64d
fergoth <fergothew@gmail.com>
2025-08-19 14:25:47 +0300

    change custom exception to raise_for_status

--- star_api_requests.py
+++ star_api_requests.py
@@ -2,8 +2,6 @@ from io import BytesIO
 from urllib.parse import urljoin
 
 import requests
-from dotenv import load_dotenv
-
 
 def get_products(url, token, documentId=None):
     url = urljoin(url, "api/products/")
@@ -13,46 +11,39 @@ def get_products(url, token, documentId=None):
         params["populate"] = "picture"
     headers = {"Authorization": f"Bearer {token}"}
     response = requests.get(url, headers=headers, params=params)
-    if response.status_code == 200:
-        return response.json()["data"]
-    else:
-        raise Exception(f"Ошибка: {response.status_code, response.text}")
-
+    response.raise_for_status()
+    return response.json()["data"]
 
 def get_picture(url, token, picture_url):
     url = urljoin(url, picture_url)
     headers = {"Authorization": f"Bearer {token}"}
     response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        return BytesIO(response.content)
-    else:
-        raise Exception(f"Ошибка: {response.status_code, response.text}")
+    response.raise_for_status()
+    return BytesIO(response.content)
+
 
 
 def get_cart(url, token, tg_user_id):
     url = urljoin(url, "api/carts/")
     headers = {"Authorization": f"Bearer {token}"}
     response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        raise Exception(f"Ошибка: {response.status_code, response.text}")
-
+    response.raise_for_status()
+    return response.json()
 
 def get_or_create_cart(url, token, tg_user_id):
     url = urljoin(url, "api/carts/")
     headers = {"Authorization": f"Bearer {token}"}
     params = {"filters[tg_user_id][$eq]": tg_user_id}
     response = requests.get(url, headers=headers, params=params)
-    if response.status_code == 200:
-        carts = response.json()["data"]
-        if not carts:
-            data = {"data": {"tg_user_id": tg_user_id}}
-            response = requests.post(url, headers=headers, json=data)
-            if response.status_code == 201:
-                return response.json()["data"]["documentId"]
-        else:
-            return carts[0]["documentId"]
+    response.raise_for_status()
+    carts = response.json()["data"]
+    if not carts:
+        data = {"data": {"tg_user_id": tg_user_id}}
+        response = requests.post(url, headers=headers, json=data)
+        response.raise_for_status()
+        return response.json()["data"]["documentId"]
+    else:
+        return carts[0]["documentId"]
 
 
 def add_product_to_cart(
@@ -77,6 +68,7 @@ def add_product_to_cart(
         }
 
         response = requests.put(url, headers=headers, json=data)
+        response.raise_for_status()
     else:
         url = urljoin(url, "api/product-carts/")
         headers = {"Authorization": f"Bearer {token}"}
@@ -89,12 +81,8 @@ def add_product_to_cart(
         }
 
         response = requests.post(url, headers=headers, json=data)
-        if response.status_code == 201:
-            return response.json()["data"]["documentId"]
-        else:
-            raise Exception(
-                f"Ошибка добавления товара в корзину: {response.status_code, response.text}"
-            )
+        response.raise_for_status()
+        return response.json()["data"]["documentId"]
 
 
 def get_cart_products(url, token, user_tg_id):
@@ -105,13 +93,8 @@ def get_cart_products(url, token, user_tg_id):
         "populate[0]": "product",
     }
     response = requests.get(url, headers=headers, params=params)
-    if response.status_code == 200:
-        product_cart = response.json()["data"]
-        return product_cart
-    else:
-        raise Exception(
-            f"Ошибка получения товаров в корзине: {response.status_code, response.text}"
-        )
+    response.raise_for_status()
+    return response.json()["data"]
 
 
 def delete_from_cart(url, token, product_cart_document_id):
@@ -120,10 +103,7 @@ def delete_from_cart(url, token, product_cart_document_id):
     )
     headers = {"Authorization": f"Bearer {token}"}
     response = requests.delete(url, headers=headers)
-    if response.status_code != 204:
-        raise Exception(
-            f"Ошибка удаления товара из корзины: {response.status_code, response.text}"
-        )
+    response.raise_for_status()
 
 
 def add_client_email(url, token, email, tg_user_id):
@@ -131,8 +111,4 @@ def add_client_email(url, token, email, tg_user_id):
     headers = {"Authorization": f"Bearer {token}"}
     data = {"data": {"email": email, "tg_user_id": str(tg_user_id)}}
     response = requests.post(url, headers=headers, json=data)
-    print(response.status_code, response.text)
-    if response.status_code != 201:
-        raise Exception(
-            f"Ошибка добавления email клиента: {response.status_code, response.text}"
-        )
+    response.raise_for_status()
